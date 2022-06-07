d87f8f046113561e758f7739e1814f563d1ac79b
Jordan Kramer <45769925+blinKAlliance@users.noreply.github.com>
2022-06-07 14:22:21 -0500

    Replace Image with FlagImage view that renders one flag using the specific set of modifiers we had for project 3

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -7,6 +7,17 @@
 
 import SwiftUI
 
+struct FlagImage: View {
+    var imageFileName: String
+    
+    var body: some View {
+        Image(imageFileName)
+            .renderingMode(.original)
+            .clipShape(Capsule())
+            .shadow(radius: 5)
+    }
+}
+
 struct ContentView: View {
     @State private var showingScore = false
     @State private var resetScore = false
@@ -43,10 +54,7 @@ struct ContentView: View {
                         Button {
                             flagTapped(number)
                         } label: {
-                            Image(countries[number])
-                                .renderingMode(.original)
-                                .clipShape(Capsule())
-                                .shadow(radius: 5)
+                            FlagImage(imageFileName: countries[number])
                         }
                     }
                 }
